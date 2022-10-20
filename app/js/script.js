6c1c09dbbc3ff052d6ac282ecf1dbf6a586b15d7
markdzhurko <mark.dzhurko@gmail.com>
2022-10-20 14:54:06 +0300

    fixed modal form

--- app/js/script.js
+++ app/js/script.js
@@ -25,15 +25,23 @@ $('.achievements__list').slick({
 
 const buttonForm = document.querySelector('.it__body-btn');
 const form = document.querySelector('.it__form');
-const close = document.querySelector('.it__form-close')
+const close = document.querySelector('.it__form-close');
+const overlay = document.querySelector('.it__form-overlay');
+const body = document.querySelector ('body')
 
 buttonForm.addEventListener('click', menuToggle);
 close.addEventListener('click', menuRemove);
+overlay.addEventListener('click', menuRemove);
+
 
 function menuToggle() {
     form.classList.toggle('show');
+    overlay.classList.toggle('show');
+    body.classList.toggle('no-scroll');
 }
 function menuRemove() {
     form.classList.remove('show');
+    overlay.classList.remove('show');
+    body.classList.remove('no-scroll');
 }
-console.log(form)
+console.log(form, overlay, body)
