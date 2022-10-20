57d7e1ae0ab81508dec385717fcca37dce4b2e27
markdzhurko <mark.dzhurko@gmail.com>
2022-10-20 17:12:02 +0300

    Fixed burger menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,3 +1,4 @@
+
 $('.achievements__list').slick({
     infinite: true,
     arrows: false,
@@ -6,60 +7,67 @@ $('.achievements__list').slick({
     dots: true,
     responsive: [
         {
-          breakpoint: 648,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-          }
+            breakpoint: 648,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
         }
     ]
-  });
+});
 
-  $('.team').slick({
+$('.team').slick({
     infinite: true,
     arrows: false,
     slidesToShow: 1,
     slidesToScroll: 1,
     dots: true,
-  });
-
+});
 
 const burger = document.querySelector('.header__menu-btn');
 const burgerMenu = document.querySelector('.header__list');
 const burgerClose = document.querySelector('.header__list-close');
-const overlay = document.querySelector('.it__form-overlay');
-const buttonForm = document.querySelector('.it__body-btn');
-const form = document.querySelector('.it__form');
-const close = document.querySelector('.it__form-close');
+const body = document.querySelector('body');
 const overlayHeader = document.querySelector('.header__overlay');
-const body = document.querySelector ('body');
 
 burger.addEventListener('click', burgerToggle);
 burgerClose.addEventListener('click', burgerRemove);
-buttonForm.addEventListener('click', menuToggle);
-close.addEventListener('click', menuRemove);
-overlay.addEventListener('click', menuRemove);
 overlayHeader.addEventListener('click', burgerRemove);
 
-
-function menuToggle() {
-    form.classList.toggle('show');
-    overlay.classList.toggle('show');
-    body.classList.toggle('no-scroll');
-}
-function menuRemove() {
-    form.classList.remove('show');
-    overlay.classList.remove('show');
-    body.classList.remove('no-scroll');
-}
 function burgerToggle() {
     burgerMenu.classList.toggle('show');
     overlayHeader.classList.toggle('show');
+    body.classList.toggle('no-scroll');
 }
-
 function burgerRemove() {
     burgerMenu.classList.remove('show');
     overlayHeader.classList.remove('show');
     body.classList.remove('no-scroll');
 }
 
+
+(function () {
+    const overlay = document.querySelector('.it__form-overlay');
+    const buttonForm = document.querySelector('.it__body-btn');
+    const form = document.querySelector('.it__form');
+    const close = document.querySelector('.it__form-close');
+    const body = document.querySelector('body');
+
+    if(!overlay) return
+
+    buttonForm.addEventListener('click', menuToggle);
+    close.addEventListener('click', menuRemove);
+    overlay.addEventListener('click', menuRemove);
+
+
+    function menuToggle() {
+        form.classList.toggle('show');
+        overlay.classList.toggle('show');
+        body.classList.toggle('no-scroll');
+    }
+    function menuRemove() {
+        form.classList.remove('show');
+        overlay.classList.remove('show');
+        body.classList.remove('no-scroll');
+    }
+})()
